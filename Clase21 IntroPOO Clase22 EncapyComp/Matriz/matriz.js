a01464aa1522b7b76b5a60456224f17c6e7c28c8
ccollado0307 <ccollado0307@gmail.com>
2020-08-09 12:08:52 -0300

    Ejercicios Clase 21 y 22

--- Clase21 IntroPOO Clase22 EncapyComp/Matriz/matriz.js
+++ Clase21 IntroPOO Clase22 EncapyComp/Matriz/matriz.js
@@ -27,42 +27,47 @@ var Nodo = /** @class */ (function () {
     };
     return Nodo;
 }());
-//Esta funcion devuelve el valor en la posicion (i,j)
-var getElem = function (fila, columna, matriz) {
-    var nodo = matriz[matriz.length - 1]; //Tomo el ultimo NODO para saber la cant de FILAS y COLUMNAS
-    var i = nodo.getFila();
-    var j = nodo.getColumna();
-    var pos = -1; //Lo utilizo para moverme dentro del arreglo 
-    for (var iCont = 0; iCont <= i; iCont++) {
-        pos++;
-        if (iCont == fila) {
-            for (var jCont = 0; jCont <= j; jCont++) {
-                nodo = matriz[pos];
-                if (jCont == columna) {
-                    return nodo.getValor();
+var Matriz = /** @class */ (function () {
+    function Matriz() {
+        var _this = this;
+        //Esta funcion recibe como parametro la matriz e instancia a la Clase Matriz 
+        this.cargarMatriz = function (arreglo) {
+            var arregloColumna;
+            for (var i = 0; i < arreglo.length; i++) {
+                arregloColumna = arreglo[i];
+                for (var j = 0; j < arregloColumna.length; j++) {
+                    var nodo = new Nodo(i, j, arregloColumna[j]);
+                    _this.matriz.push(nodo);
                 }
-                pos++; //Adelanto una fila
             }
-        }
-        else {
-            pos = pos + j; //Adelanto todas la columnas
-        }
+        };
+        this.getElem = function (fila, columna) {
+            var nodo = _this.matriz[_this.matriz.length - 1]; //Tomo el ultimo NODO para saber la cant de FILAS y COLUMNAS
+            var i = nodo.getFila();
+            var j = nodo.getColumna();
+            var pos = -1; //Lo utilizo para moverme dentro del arreglo 
+            for (var iCont = 0; iCont <= i; iCont++) {
+                pos++;
+                if (iCont == fila) {
+                    for (var jCont = 0; jCont <= j; jCont++) {
+                        nodo = _this.matriz[pos];
+                        if (jCont == columna) {
+                            return nodo.getValor();
+                        }
+                        pos++; //Adelanto una fila
+                    }
+                }
+                else {
+                    pos = pos + j; //Adelanto todas la columnas
+                }
+            }
+            return -1;
+        };
+        this.matriz = [];
     }
-    return -1;
-};
-//Esta funcion recibe como parametro la matriz e instancia a la Clase Matriz 
-var cargarMatriz = function (arreglo) {
-    var matriz = [];
-    var arregloColumna;
-    for (var i = 0; i < arreglo.length; i++) {
-        arregloColumna = arreglo[i];
-        for (var j = 0; j < arregloColumna.length; j++) {
-            var nodo = new Nodo(i, j, arregloColumna[j]);
-            matriz.push(nodo);
-        }
-    }
-    return matriz;
-};
+    return Matriz;
+}());
+//Esta funcion devuelve el valor en la posicion (i,j)
 //Esta funcion recibe como paramaetro la cant Filas y Columnas y devuelve una matriz
 var cargarArreglo = function (cantF, cantC) {
     var arregloF = new Array(cantF);
@@ -80,9 +85,10 @@ var aux1 = readlineSync.questionInt('Ingrese la Cantidad de Filas ');
 var aux2 = readlineSync.questionInt('Ingrese la Cantidad de Columnas ');
 var arregloPrueba = cargarArreglo(aux1, aux2);
 console.log('La MATRIZ esta dada por los pares ', arregloPrueba);
-var matriz = cargarMatriz(arregloPrueba);
+var matriz = new Matriz;
+matriz.cargarMatriz(arregloPrueba);
 console.log('La MATRIZ esta dada por los nodos ', matriz);
 aux1 = readlineSync.questionInt('Ingrese la Fila que desea consultar ');
 aux2 = readlineSync.questionInt('Ingrese la Columna que desea consultar ');
-var valor = getElem(aux1, aux2, matriz);
+var valor = matriz.getElem(aux1, aux2);
 console.log('El VALOR ', valor, 'es el que corresponde a la posicion [', aux1, ',', aux2, ']');
--- Clase21 IntroPOO Clase22 EncapyComp/Matriz/matriz.ts
+++ Clase21 IntroPOO Clase22 EncapyComp/Matriz/matriz.ts
@@ -5,7 +5,7 @@ class Nodo {
     private columna: number;
     private valor: number;
     
-    constructor(fila: number, columna: number, valor: number) {
+    public constructor(fila: number, columna: number, valor: number) {
         this.fila = fila;
         this.columna = columna;
         this.valor = valor;
@@ -35,48 +35,54 @@ class Nodo {
         this.valor = valor;
     }
 }
+class Matriz {
+    private matriz: Array<Nodo>;
 
-//Esta funcion devuelve el valor en la posicion (i,j)
-let getElem = (fila: number, columna: number, matriz: Array<Nodo>): number => { 
-    let nodo: Nodo = matriz[matriz.length-1]; //Tomo el ultimo NODO para saber la cant de FILAS y COLUMNAS
-    
-    let i: number = nodo.getFila();
-    let j: number = nodo.getColumna();
-    
-    let pos: number = -1;//Lo utilizo para moverme dentro del arreglo 
-    for (let iCont: number = 0; iCont <= i; iCont++) {
-        pos++; 
-        if (iCont == fila) {
-            for (let jCont = 0; jCont <= j; jCont++) {
-                nodo = matriz[pos];
-                if (jCont == columna) {
-                    return nodo.getValor();
-                }
-                pos++; //Adelanto una fila
+    public constructor() {
+        this.matriz = [];
+    }
+
+    //Esta funcion recibe como parametro la matriz e instancia a la Clase Matriz 
+    public cargarMatriz = (arreglo: Array<Array<number>>) => { 
+        let arregloColumna: Array<number>;
+        for (let i: number = 0; i < arreglo.length; i++) {
+            arregloColumna = arreglo[i];
+
+            for (let j: number = 0; j < arregloColumna.length; j++) {
+                let nodo: Nodo = new Nodo(i,j,arregloColumna[j]);
+                this.matriz.push(nodo);
             }
         }
-        else {
-            pos = pos + j;//Adelanto todas la columnas
-        }
     }
-    return -1;
+
+    public getElem = (fila: number, columna: number): number => { 
+        let nodo: Nodo = this.matriz[this.matriz.length-1]; //Tomo el ultimo NODO para saber la cant de FILAS y COLUMNAS
+        
+        let i: number = nodo.getFila();
+        let j: number = nodo.getColumna();
+        
+        let pos: number = -1;//Lo utilizo para moverme dentro del arreglo 
+        for (let iCont: number = 0; iCont <= i; iCont++) {
+            pos++; 
+            if (iCont == fila) {
+                for (let jCont = 0; jCont <= j; jCont++) {
+                    nodo = this.matriz[pos];
+                    if (jCont == columna) {
+                        return nodo.getValor();
+                    }
+                    pos++; //Adelanto una fila
+                }
+            }
+            else {
+                pos = pos + j;//Adelanto todas la columnas
+            }
+        }
+        return -1;
+    }
 }
 
-//Esta funcion recibe como parametro la matriz e instancia a la Clase Matriz 
-let cargarMatriz = (arreglo: Array<Array<number>>): Array<Nodo>  => { 
-    let matriz: Array<Nodo>  = [];
+//Esta funcion devuelve el valor en la posicion (i,j)
 
-    let arregloColumna: Array<number>;
-    for (let i: number = 0; i < arreglo.length; i++) {
-        arregloColumna = arreglo[i];
-
-        for (let j: number = 0; j < arregloColumna.length; j++) {
-            let nodo: Nodo = new Nodo(i,j,arregloColumna[j]);
-            matriz.push(nodo);
-        }
-    }
-    return matriz;
-}
 
 //Esta funcion recibe como paramaetro la cant Filas y Columnas y devuelve una matriz
 let cargarArreglo = (cantF: number, cantC: number): Array<Array<number>> => { 
@@ -99,10 +105,11 @@ let aux2: number = readlineSync.questionInt('Ingrese la Cantidad de Columnas ');
 let arregloPrueba = cargarArreglo(aux1,aux2);
 console.log('La MATRIZ esta dada por los pares ', arregloPrueba);
 
-let matriz: Array<Nodo> = cargarMatriz(arregloPrueba);
+let matriz: Matriz = new Matriz;
+matriz.cargarMatriz(arregloPrueba) 
 console.log('La MATRIZ esta dada por los nodos ', matriz);
 
 aux1 = readlineSync.questionInt('Ingrese la Fila que desea consultar ');
 aux2 = readlineSync.questionInt('Ingrese la Columna que desea consultar ');
-let valor: number = getElem(aux1,aux2,matriz);
+let valor: number = matriz.getElem(aux1,aux2);
 console.log('El VALOR ', valor, 'es el que corresponde a la posicion [', aux1,',',aux2,']');
