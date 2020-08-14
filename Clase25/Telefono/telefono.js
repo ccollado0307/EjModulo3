721e48bbe3fe015853b3eea572c73990988caed5
ccollado0307 <ccollado0307@gmail.com>
2020-08-14 20:56:16 -0300

    Actualizacion

--- Clase25/Telefono/telefono.js
+++ Clase25/Telefono/telefono.js
@@ -35,9 +35,17 @@ var Telefono = /** @class */ (function () {
 }());
 var Camara = /** @class */ (function () {
     function Camara() {
+        this.pixels = '20px';
     }
+    ;
     Camara.prototype.sacarFoto = function () {
         console.log('Saco una FOTO');
+    };
+    Camara.prototype.getPixels = function () {
+        return this.pixels;
+    };
+    Camara.prototype.modifPixeles = function (pixels) {
+        this.pixels = pixels;
     };
     return Camara;
 }());
@@ -54,8 +62,16 @@ var TelefonoConCamara = /** @class */ (function (_super) {
     return TelefonoConCamara;
 }(Telefono));
 var telefono = new Telefono;
+telefono.prenderApagar();
 telefono.mandarMensaje('Hola soy un telefono', 154604600);
+telefono.hacerLlamada(2494123123);
 var telefonoConCamara = new TelefonoConCamara;
 telefonoConCamara.prenderApagar();
 telefonoConCamara.sacarFoto();
+telefonoConCamara.mandarMensaje('Soy un TE', 2494567567);
 console.log('Telefono con Camara ', telefonoConCamara);
+var camara = new Camara;
+camara.sacarFoto();
+camara.modifPixeles('30px');
+console.log(camara.getPixels());
+console.log(telefonoConCamara);
